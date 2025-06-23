7f6eaf9846631f305a0bc714eb13fb78f71615b4
durga_appscrip <durga@appscrip.co>
2025-06-23 16:25:46 +0530

    fix: update lock.json

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+echo "📂 Current directory:"
+pwd
+
+echo "📁 Listing files:"
+ls -al
+
+echo "📄 Showing package-lock.json head:"
+head -n 20 package-lock.json
+
 npm install
 npm run build:pages
